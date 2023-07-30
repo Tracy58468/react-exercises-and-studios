d3ccf218d5533c81fae60fb56222857ce9887d96
Tracy H <aag9n17o@duck.com>
2023-07-29 19:16:02 -0500

    Added books

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book LIst";
+   let book1 = "https://mpd-biblio-covers.imgix.net/9781250826794.jpg?w=450";
+   let book2 = "https://mpd-biblio-covers.imgix.net/9781250196552.jpg?w=450";
+   let book3 = "https://mpd-biblio-covers.imgix.net/9781250213594.jpg?w=450";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Witch King, by Martha Wells" />
+         <img src={book2} alt="The Book of Gems, by Fran Wilde" />
+         <img src={book3} alt="Across the Green Grass Fields, by Seanan McGuire" />
       </div>      
    );
 }
