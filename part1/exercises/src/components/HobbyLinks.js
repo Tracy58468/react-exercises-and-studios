a55769883be918b504a3fa16fc297964376e8f7c
Tracy H <aag9n17o@duck.com>
2023-07-29 19:37:41 -0500

    Add hobbies.

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,10 @@
+export default function HobbyLinks () {
+ let hobbyLinks = ["https://publishing.tor.com/", "https://wordpress.org"]
+ return (
+    <>
+       <h3>Hobbies</h3>
+          <a href = {hobbyLinks[0]}>Reading</a><br />
+          <a href = {hobbyLinks[1]}>WordPress</a>
+    </>
+ );
+}
