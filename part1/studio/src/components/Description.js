418dfdfded020ab7c037bc2832b2090740fbcf07
Tracy H <aag9n17o@duck.com>
2023-08-03 19:42:17 -0500

    halfway-ish through studio

---
+++ part1/studio/src/components/Description.js
@@ -0,0 +1,33 @@
+import styles from './Description.module.css';
+import React from 'react';
+
+function RecipeAuthor() {
+  let authorName = "Alton Brown";
+  let authorLink = "https://www.foodnetwork.com/recipes/alton-brown/crepes-recipe-1911037";
+  let authorPhoto = "https://food.fnr.sndimg.com/content/dam/images/food/editorial/talent/alton-brown/FN-TalentAvatar-Alton-Brown.jpg.rend.hgtvcom.126.126.suffix/1532979092729.jpeg";
+  return(
+    <div className = {StyleSheet.recipeAuthorBlock}>
+      <img src = {authorPhoto} alt = "Alton Brown" className = {StyleSheet.imageUpdates} />
+      <div>
+        <h3>{authorName}</h3>
+        <a href = {authorLink}>Food Network - Alton Brown - Crepes</a>
+      </div>
+    </div>
+  )
+};
+
+class RecipeDescription extends React.Component {
+  render() {
+    return(
+      <div> 
+        <div>
+          <h1>Crepes</h1>
+          <p>Ummy, nummy crepes</p>
+        </div>
+        <RecipeAuthor />
+      </div>
+    );    
+  }
+};
+
+export default RecipeDescription;
